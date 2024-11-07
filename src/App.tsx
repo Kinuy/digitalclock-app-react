bdde03e1447a25129c8f56d8511bde4b3a28c761
Tobias Weber <toby-weber@gmx.de>
2024-11-07 17:03:33 +0100

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+import DigitalClock from "./component/DigitalClock.tsx";
+
+export default function App() {
+
+
+  return (
+    <>
+        <DigitalClock/>
+    </>
+  )
+}
+
